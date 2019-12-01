aaca3ae815d7f387e9f737216ee0c0cd9b6aacb2
TakeroxD <takeroxd@gmail.com>
2019-11-30 20:54:00 -0600

    gg

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const connectionUrl = 'mongodb+srv://admin:admin@clustertaco-socc1.mongodb.net/mygamelistwa?retryWrites=true&w=majority'
+const secret = 'Th1sIsS0Fuc**1ngS3cretHah4JA'
+
+module.exports={connectionUrl,secret}
--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
-const connectionURL = 'mongodb+srv://admin:admin@clustertaco-socc1.mongodb.net/mygamelistwa?retryWrites=true&w=majority'
+
+var connectionURL = process.env.DATABASE_URL || require('../config.js').connectionUrl
 
 mongoose.connect(connectionURL,{
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedtopology: true
-}).then(function(data){
-	console.log(data)
+}).then(function(){
+	console.log("Connected to DB successful")
 }).catch(function(error){
 	console.log(error)
 })
